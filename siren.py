08e674679414c8f4b04ef626e3bae23eca575273
haliphax <haliphax@gmail.com>
2021-04-19 03:08:39 -0500

    initial commit

---
+++ siren.py
@@ -0,0 +1,24 @@
+# stdlib
+from os import environ
+# 3rd party
+from flask import abort, Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+pwd = environ.get('YT_SIREN_PASSWORD', 'changeme')
+
+
+@app.route('/', methods=['POST'])
+def post():
+    if request.json['pwd'] != pwd:
+        abort(403)
+
+    with open('song.txt', 'w') as f:
+        f.write(request.json['song'])
+
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8443)
