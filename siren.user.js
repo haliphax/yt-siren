b8bb4100ceed3bee443634a00524585bcd759b1c
haliphax <haliphax@gmail.com>
2023-12-21 16:45:15 -0600

    moar resilient?

--- siren.user.js
+++ siren.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         YT Siren
-// @namespace    https://roadha.us
-// @version      0.6
-// @description  Reports current YouTube video and chapter on change
 // @author       haliphax
-// @match        https://www.youtube.com/watch?v=*
-// @match        https://music.youtube.com/watch?v=*
-// @match        https://music.youtube.com/
-// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @description  Reports current YouTube video and chapter on change
+// @grant        GM_getValue
 // @grant        GM_registerMenuCommand
 // @grant        GM_setValue
-// @grant        GM_getValue
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @match        https://music.youtube.com/
+// @match        https://music.youtube.com/watch?v=*
+// @match        https://www.youtube.com/watch?v=*
+// @namespace    https://roadha.us
+// @version      0.7
 // ==/UserScript==
 
 (async () => {
@@ -58,7 +58,7 @@
         songId = extract.exec(window.location.href)[1];
         songTitle = document.querySelector(
             window.location.hostname === "music.youtube.com"
-                ?  ".content-info-wrapper.ytmusic-player-bar "
+                ? ".content-info-wrapper.ytmusic-player-bar "
                     + "yt-formatted-string.title"
                 : "h1.title:not(.meta)"
         ).innerText;
@@ -103,7 +103,7 @@
         const chapter = document.querySelector(".ytp-chapter-title-content");
 
         if (channel === null || video === null) {
-            setTimeout(init, 100);
+            setTimeout(initStandard, 100);
             return;
         }
 
@@ -132,15 +132,23 @@
 
     // music.youtube.com
     const initMusic = async () => {
-        const artist = document.querySelector(
+        const song = document.querySelector(
             ".content-info-wrapper.ytmusic-player-bar yt-formatted-string.title"
         );
-        const song = document.querySelector(
-            ".content-info-wrapper.ytmusic-player-bar "
-            + "yt-formatted-string.title"
+        const artist = document.querySelector(
+            "yt-formatted-string.byline.ytmusic-player-bar "
+            + "a.yt-simple-endpoint.yt-formatted-string:first-child"
         );
 
-        if (artist === null || song === null) {
+        if (song === null || artist === null) {
+            setTimeout(initMusic, 100);
+            return;
+        }
+
+        await fetchChannelTitle(false);
+        await fetchSongTitle(false);
+
+        if (channelTitle === null || songTitle === null) {
             setTimeout(initMusic, 100);
             return;
         }
@@ -150,29 +158,13 @@
         new MutationObserver(fetchSongTitle).observe(
             song, { childList: true, subtree: true });
 
-        await fetchChannelTitle(false);
-        await fetchSongTitle(false);
-
-        if (channelTitle === null || songTitle === null) {
-            setTimeout(init, 100);
-            return;
-        }
-
         console.log("YT Siren: active (ytmusic)");
 
         await fetchChannelTitle();
     };
 
-    const startup = () => {
-        try {
-            window.location.hostname === "music.youtube.com"
-                ? initMusic()
-                : initStandard();
-        }
-        catch (e) {
-            setTimeout(startup, 1000);
-        }
-    };
-
-    startup();
+    console.log("YT Siren: starting up");
+    window.location.hostname === "music.youtube.com"
+        ? initMusic()
+        : initStandard();
 })();
